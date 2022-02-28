932a401684d73be6b30a03bdc596af8d665c970e
Rowan Dash <pokemonmaster9967@gmail.com>
2022-02-28 20:56:27 +0000

    Cleanup
    Remove extraneous commented stuff and functions, log processing is a work in progress

--- examples/Swacky.py
+++ examples/Swacky.py
@@ -28,85 +28,9 @@ async def on_message(message):
 
     log(message)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send("Don't mind me!")
-
-    if message.content.startswith('$help'):
-        return
-
-    """ if message.content.startswith('$grab'):
-        command = message.content[6:]
-        print(command)
-        dates = command.split(' ')
-        print(dates)
-        for date in dates:
-            datetime().strptime(date, '%d %m %Y') """
-
-    """ if message.content.startswith('$quit'):
-        await message.channel.send("Goodbye!")
-        await client.close()
-        exit(0) """
-
-    """ if message.content.startswith('$get_method'):
-        search = ""
-        find = str(message.content[12:])
-        find = find.lower()
-
-        search = "SELECT id FROM methods WHERE title LIKE '%" + \
-            find + "%'"
-
-        print(search)
-
-        # print(message.content)
-
-        await message.channel.send("Searching for methods...")
-        result = db.execute(search).fetchall()
-        if result == []:
-            await message.channel.send("No methods found.")
-            return
-        elif len(result) == 1:
-            await message.channel.send("Found 1 method.")
-        else:
-            await message.channel.send("Found " + str(len(result)) + " methods.")
-            await message.channel.send("Here are all of the methods:")
-        for i in result:
-            await message.channel.send("https://complib.org/method/" +
-                                       str(i[0])) """
-
-    if message.content.startswith('$ping'):
-        await message.channel.send('Pong!')
-        await message.channel.send(file=discord.File('pong.gif'))
-        """Get the bot's current websocket and API latency."""
-        start_time = time.time()
-        message = await message.channel.send("Testing Ping...")
-        end_time = time.time()
-
-        await message.edit(content=f"Pong! {round(client.latency * 1000)}ms\nAPI: {round((end_time - start_time) * 1000)}ms")
-
-    if message.content.startswith('!give_admin'):
-        await message.reply(file=discord.File('no.gif'))
-
-    if message.content.startswith('!yoda'):
-        content = message.content[6:]
-        q = requests.get(
-            "http://api.funtranslations.com/translate/yoda?text=" + content)
-        print(q.json())
-        await message.reply(q.json()['contents']['translated'])
-
-    if message.content.startswith('!time'):
-        date_time_now = datetime.now()
-        await message.channel.send(date_time_now)
-
-    if 'burger king' in message.content:
-        await message.reply('Don\'t say that around me.')
-
-    if message.content.startswith('!vertify'):
-        splitMsg = message.content.split(' ')
-        for word in splitMsg[1:]:
-            await message.channel.send(word)
-
-    if message.content.startswith('!xkcd'):
-        await message.reply("https://xkcd.com/")
+    if message.content.startswith('#swack process_log'):
+        processLog()
+        await message.reply("Processed Log")
 
 
 def log(message):
@@ -120,8 +44,6 @@ def log(message):
     except:
         pass
 
-    # print(df)
-
     data = df.append({'content': message.content, 'author': message.author.name,
                       'timestamp': message.created_at}, ignore_index=True)
 
@@ -134,6 +56,27 @@ def createFile():
             f.write('content,author,timestamp\n')
 
 
+def processLog():
+    df = pd.read_csv('log.csv')
+    print(len(df))
+    for i in range(len(df)):
+        print(df.iloc[i]['content'])
+        print(df.iloc[i]['author'])
+        print(df.iloc[i]['timestamp'])
+        print("\n")
+
+    authors = []
+    strings = []
+    # store the author and message content in a text file, each author is only allowed to have one message per day
+    for i in range(len(df)):
+        if not authors.__contains__(df.iloc[i]['author']):
+            authors.append(df.iloc[i]['author'])
+            strings.append(df.iloc[i]['content'])
+
+    print(authors)
+    print(strings)
+
+
 def getToken():
     token = ""
     with open('token.txt', 'r') as f:
@@ -141,5 +84,4 @@ def getToken():
     return token
 
 
-# Required to run the bot, make sure to replace <YOUR_API_TOKEN>!
 client.run(getToken())
